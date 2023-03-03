9aa32bd987361a0b9d36ae44e352ba48785fa64c
dannydms <Shadaniels1997@gmail.com>
2023-03-02 21:46:22 -0800

    TaskHOT FIX

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void printf_str(void) __attribute__((constructor));
+
+/**
+ * printf_str - prints a sentence before the main
+*/
+
+void printf_str(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
